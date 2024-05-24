88b8bf27eb326b06f0eab3ef2c8893d425c7b38c
Irwan Nawawi <muhammadirwannawawi@gmail.com>
2024-05-25 01:04:30 +0700

    modifikasi halaman onformasi dan beranda

--- flutter_reservasi_app/lib/screens/beranda_screen.dart
+++ flutter_reservasi_app/lib/screens/beranda_screen.dart
@@ -8,53 +8,117 @@ class beranda extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<String> imgList = [
       'images/img1.jpg',
-      'images/img9.jpg',
+      'images/img2.jpg',
       'images/img3.jpg',
       'images/img4.jpg',
     ];
 
     return ListView(
       children: <Widget>[
+        SizedBox(
+            height: 16), // Memberikan jarak antara slider pertama dan AppBar
         // Slider Gambar Pertama
-        CarouselSlider(
-          options: CarouselOptions(
-            height: 200.0,
-            autoPlay: true,
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          child: CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              viewportFraction: 0.8,
+              enlargeCenterPage: true,
+            ),
+            items: imgList
+                .map((item) => Container(
+                      margin: EdgeInsets.all(5.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        child:
+                            Image.asset(item, fit: BoxFit.cover, width: 1000),
+                      ),
+                    ))
+                .toList(),
           ),
-          items: imgList
-              .map((item) => Container(
-                    child: Center(
-                      child: Image.asset(item, fit: BoxFit.cover, width: 1000),
-                    ),
-                  ))
-              .toList(),
         ),
         SizedBox(height: 20),
         // Slider Gambar Kedua
-        CarouselSlider(
-          options: CarouselOptions(
-            height: 200.0,
-            autoPlay: true,
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          child: CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              viewportFraction: 0.8,
+              enlargeCenterPage: true,
+            ),
+            items: imgList
+                .map((item) => Container(
+                      margin: EdgeInsets.all(5.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        child:
+                            Image.asset(item, fit: BoxFit.cover, width: 1000),
+                      ),
+                    ))
+                .toList(),
           ),
-          items: imgList
-              .map((item) => Container(
-                    child: Center(
-                      child: Image.asset(item, fit: BoxFit.cover, width: 1000),
-                    ),
-                  ))
-              .toList(),
         ),
         SizedBox(height: 20),
-        // Gambar-gambar Biasa
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'Nikmati pemandangan alam pilihan :',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 10),
+        // Gambar Non-Slider Pertama dengan Deskripsi
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            children: <Widget>[
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
               Image.asset('images/img5.jpg', height: 150),
+              SizedBox(height: 5),
+              Text(
+                'Deskripsi singkat untuk gambar pertama.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 20),
+        // Gambar-gambar Biasa dengan Deskripsi
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Image.asset('images/img6.jpeg', height: 150),
+                  SizedBox(height: 5),
+                  Text(
+                    'Deskripsi singkat untuk gambar kedua.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ],
+              ),
               SizedBox(height: 10),
-              Image.asset('images/img6.jpeg', height: 150),
-              SizedBox(height: 10),
-              Image.asset('images/img7.jpg', height: 150),
+              Column(
+                children: [
+                  Image.asset('images/img7.jpg', height: 150),
+                  SizedBox(height: 5),
+                  Text(
+                    'Deskripsi singkat untuk gambar ketiga.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
--- flutter_reservasi_app/lib/screens/informasi_screen.dart
+++ flutter_reservasi_app/lib/screens/informasi_screen.dart
@@ -2,10 +2,89 @@ import 'package:flutter/material.dart';
 
 class informasi extends StatelessWidget {
   const informasi({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Halaman Informasi'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Informasi Aplikasi Reservasi',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              '1. Aplikasi dapat diakses melalui halaman tiket di navigasi bar bawah atau di pojok kanan atas halaman.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '2. Setelah masuk ke halaman reservasi, silakan pilih jenis tiket yang akan Anda beli: dewasa serta balita.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '3. Tunggu aplikasi melakukan verifikasi ketersediaan stok tiket ke database sistem oleh Admin.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '4. Jika stok tiket tidak tersedia, akan muncul alert peringatan dan Anda akan diarahkan kembali ke halaman beranda.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '5. Jika stok tiket tersedia, Anda akan diarahkan ke halaman pengisian identitas reservasi. Pastikan untuk mengisi dengan benar dan klik tombol "Lanjutkan".',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '6. Selanjutnya, akan muncul halaman detail pesanan. Periksa kembali isian identitas dan total pesanan tiket, lalu klik tombol "Lanjutkan".',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '7. Pilih metode pembayaran yang diinginkan, bisa melalui transfer bank atau dompet digital (Dana/LinkAja/Ovo).',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '8. Setelah melakukan pembayaran, kirimkan bukti transaksi ke nomor WhatsApp Admin (083112940959) atau melalui link berikut untuk verifikasi oleh Admin Kami.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '9. Pastikan jumlah pembayaran sesuai dengan nominal transfer. Setelah itu, klik tombol "Selesai".',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '10. Proses reservasi Anda akan berstatus "Menunggu Verifikasi" oleh Admin. Jika valid, status reservasi Anda akan menjadi "Berhasil" dengan pesan konfirmasi.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '11. Reservasi Anda selesai! Anda hanya perlu datang ke lokasi wisata dan menyebutkan nama Anda di loket tiket tanpa perlu membayar lagi.',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
